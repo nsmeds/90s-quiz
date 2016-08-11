9e7d8688bbae6b1db25b80476a4aa3dc39bf7956
nsmeds <niilosmeds@gmail.com>
2016-08-11 16:44:22 -0700

    Script in progress. Not properly looping through the questions at the moment.

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
-function cycle () {
-  var begin = document.getElementById("begin-button");
+window.onload = function () {
+  'use strict';
+  var correct = 0,
+    incorrect = 0,
+    current = 0,
+    quiz = document.getElementById('quiz'),
+    begin = document.getElementById("begin-button"),
+    fieldset = document.getElementsByTagName("fieldset")[current],
+    results = document.getElementsByClassName("results")[current],
+    submitButton = document.getElementById("submit-button"),
+    nextButton = document.getElementById("next-button"),
+    radios = document.getElementsByTagName("input")[current];
+
+  function checkAnswer() {
+    if (radios.value == "x") {
+      if (radios.checked === true) {
+        correct += 1;
+        quiz.insertAdjacentHTML('afterbegin', '<h3 id="h3">Correct!</h3>');
+      } else {
+        incorrect += 1;
+        quiz.insertAdjacentHTML('afterbegin', '<h3 id="h3">Incorrect!</h3>');
+      }
+    }
+    current += 1;
+    results.style.display = 'flex';
+    submitButton.style.display = 'none';
+    nextButton.style.display = 'flex';
+  }
+
+  
+  function submit() {
+    submitButton.addEventListener('click', function (event) {
+      event.preventDefault();
+      fieldset.style.display = 'none';
+      results.style.display = 'flex';
+      checkAnswer();
+    });
+  }
+ 
+  function displayQuiz() {
+//  for (var i = 0; i <= fieldset.length; i++) {
+    nextButton.style.display = 'none';
+    fieldset.style.display = "flex";
+    submitButton.style.display = "flex";
+    submit();
+//  }
+  }
+  
   begin.addEventListener('click', function (event) {
     document.getElementById('welcome').style.display = 'none';
-    document.getElementById('fieldset1').style.display = 'flex';
-    document.getElementById('submit-button').style.display = 'flex';
-  })
-}
+    displayQuiz();
+  });
 
-cycle();
+  nextButton.addEventListener('click', function (event) {
+    document.getElementById('h3').innerHTML = '';
+    results.style.display = 'none';
+    displayQuiz();
+  });
+};
